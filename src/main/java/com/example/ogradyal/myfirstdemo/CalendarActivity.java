37b1b02052967e7078c9adfbaf71877dd82f500f
alanna96 <ogradyal@tcd.ie>
2017-02-20 12:34:11 +0000

    Add files via upload

---
+++ src/main/java/com/example/ogradyal/myfirstdemo/CalendarActivity.java
@@ -0,0 +1,39 @@
+package com.example.ogradyal.myfirstdemo;
+
+
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.CalendarView.OnDateChangeListener;
+import android.widget.CalendarView;
+import android.widget.Toast;
+
+public class CalendarActivity extends AppCompatActivity {
+    public final static String EXTRA_EVENT = "com.example.myfirstdemo.EVENT";
+    String date;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar);
+        CalendarView calendarView = (CalendarView) findViewById(R.id.calendarView1);
+        calendarView.setOnDateChangeListener(new OnDateChangeListener() {
+
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month,
+                                            int dayOfMonth) {
+                date = "" + dayOfMonth +"_" + month; //file name
+                String day = "" +dayOfMonth;
+                Toast.makeText(getApplicationContext(), day, Toast.LENGTH_SHORT).show();// TODO Auto-generated method stub
+
+                Intent intentShow = new Intent(view.getContext(), ShowEvent.class);
+                intentShow.putExtra(EXTRA_EVENT, date);
+                startActivity(intentShow);
+
+            }
+        });
+
+    }
+
+}
